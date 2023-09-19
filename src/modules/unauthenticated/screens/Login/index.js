12f6d466ec8ff45a63df5eaa02d538b25d140eff
Alisson Moraes <alissonmoraesdev@gmail.com>
2023-09-19 01:15:44 -0300

    fix: responsividade tela de login

--- src/modules/unauthenticated/screens/Login/index.js
+++ src/modules/unauthenticated/screens/Login/index.js
@@ -8,11 +8,11 @@ export const LoginScreen = () => {
         alignItems={["center","flex-start", "space-between"]}
         justifyContent="center" 
         paddingLeft={["0px", "0px", "0px","0px","112px"]} 
-        padding={["24px","32px","48px","64px","0px"]}
+        padding={["24px","32px","48px","64px","80px"]}
         flexDir="column" 
-        w={["100%","100%","100%","50%","40%"]}
+        w={["100%","100%","100%","80%","50%"]}
         h="100%">
-        <Flex flexDirection="column" w={["100%", "100%", "100%","416px"]}>
+        <Flex flexDirection="column" w={["100%", "100%", "100%","100%","416px"]}>
           <Image src="/img/logo.svg" alt="BookClub Logo" w="160px" h="48px" />
           <Text.ScreenTitle mt="48px">Login</Text.ScreenTitle>
           <Input
@@ -30,7 +30,7 @@ export const LoginScreen = () => {
           <Link.Action mt="32px" text="Não possui uma conta?" actionText="Cadastra-se aqui" />
         </Flex>
       </Flex>
-      <Flex w={["0%","0%", "40%","50%","60%"]}h="100vh" 
+      <Flex w={["0%","0%", "30%","60%","60%"]}h="100vh" 
         backgroundImage="url('/img/auth_background.svg')"
         backgroundSize="cover"
         backgroundPosition="center"
