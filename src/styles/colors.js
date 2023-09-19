ba16597ef00286e83df36d36a2b294dabce5c977
Alisson Moraes <alissonmoraesdev@gmail.com>
2023-09-18 23:26:07 -0300

    feat: implementação chakra-ui tela de login

---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#FFC734',
+  black: '#000000',
+  white: '#FFFFFF',
+  grayDark: '#737373',
+  gray: '#E6E6E6',
+  grayLight: '#F2F1F1',
+  background: '#FEFEFE'
+}
